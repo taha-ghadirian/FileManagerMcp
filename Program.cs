62a9120e0b011b006f73af32e214e99863b8f319
Taha Ghadirian <Taha@smilecode.ir>
2025-04-07 19:22:11 +0330

    feat: Add environment variable configuration for FTP login details

--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+// Set login info from environment variables
+Login.host = Environment.GetEnvironmentVariable("FTP_HOST") ?? throw new Exception("FTP_HOST environment variable is required");
+Login.username = Environment.GetEnvironmentVariable("FTP_USERNAME") ?? throw new Exception("FTP_USERNAME environment variable is required"); 
+Login.password = Environment.GetEnvironmentVariable("FTP_PASSWORD") ?? throw new Exception("FTP_PASSWORD environment variable is required");
+if (int.TryParse(Environment.GetEnvironmentVariable("FTP_PORT"), out int port))
+{
+    Login.port = port;
+}
+
 var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(consoleLogOptions =>
 {
