7404e3a78947d6ba6ea3a65ccb086cf50ef64994
Taha Ghadirian <Taha@smilecode.ir>
2025-04-16 14:05:01 +0330

    refactor: Simplify service configuration by integrating configuration loading into ServiceConfiguration class

--- src/Program.cs
+++ src/Program.cs
@@ -4,28 +4,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 
-// Load configuration from appsettings.json
-// var configuration = new ConfigurationBuilder()
-//     .SetBasePath(Directory.GetCurrentDirectory())
-//     .AddEnvironmentVariables()
-//     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-//     .AddUserSecrets<Program>()
-//     .Build();
 
-// Use the new ServiceConfiguration class to configure services
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Configuration
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddEnvironmentVariables()
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddUserSecrets<Program>();
-
-var configuration = builder.Configuration;
+builder.ConfigureServices();
 
 var loginDetail = builder.Configuration.GetSection("FTP").Get<FtpCredential>();
-builder.Services.AddSingleton<FtpCredential>(loginDetail ?? throw new ArgumentNullException(nameof(loginDetail)));
-
-builder.ConfigureServices(configuration);
+builder.Services.AddSingleton(loginDetail ?? throw new ArgumentNullException(nameof(loginDetail)));
 
 await builder.Build().RunAsync();
--- src/ServiceConfiguration.cs
+++ src/ServiceConfiguration.cs
@@ -7,8 +7,14 @@ using FileManagerMcp.Toolkits;
 
 public static class ServiceConfiguration
 {
-    public static HostApplicationBuilder ConfigureServices(this HostApplicationBuilder builder, IConfiguration configuration)
+    public static HostApplicationBuilder ConfigureServices(this HostApplicationBuilder builder)
     {
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets<Program>();
+        
         builder.Logging.AddConsole(consoleLogOptions =>
         {
             // Configure all logs to go to stderr
