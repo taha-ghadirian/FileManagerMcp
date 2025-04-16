fcecdc358d7113eb29e678e49007c8dfa02b788a
Taha Ghadirian <Taha@smilecode.ir>
2025-04-16 13:31:20 +0330

    refactor: streamline configuration loading and add ServiceConfiguration class

--- src/Program.cs
+++ src/Program.cs
@@ -5,14 +5,23 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 
 // Load configuration from appsettings.json
-var configuration = new ConfigurationBuilder()
+// var configuration = new ConfigurationBuilder()
+//     .SetBasePath(Directory.GetCurrentDirectory())
+//     .AddEnvironmentVariables()
+//     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//     .AddUserSecrets<Program>()
+//     .Build();
+
+// Use the new ServiceConfiguration class to configure services
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddEnvironmentVariables()
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddUserSecrets<Program>()
-    .Build();
+    .AddUserSecrets<Program>();
 
-// Set login info from environment variables
+var configuration = builder.Configuration;
 Login.host = configuration["FTP_Host"] ?? throw new Exception("FTP_Host configuration is required");
 Login.username = configuration["FTP_Username"] ?? throw new Exception("FTP_Username configuration is required");
 Login.password = configuration["FTP_Password"] ?? throw new Exception("FTP_Password configuration is required");
@@ -21,24 +30,6 @@ if (int.TryParse(configuration["FTP_Port"], out int port))
     Login.port = port;
 }
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Logging.AddConsole(consoleLogOptions =>
-{
-    // Configure all logs to go to stderr
-    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
-});
-builder.Services
-    .AddMcpServer()
-    .WithStdioServerTransport()
-    .WithTools<FtpTool>();
-
-// Add configuration to services
-builder.Services.AddSingleton<IConfiguration>(configuration);
+builder.ConfigureServices(configuration);
 
 await builder.Build().RunAsync();
-
-// var app = builder.Build();
-
-// app.MapMcp();
-
-// app.Run();
---
+++ src/ServiceConfiguration.cs
@@ -0,0 +1,25 @@
+// Create a new file to configure services
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using FileManagerMcp;
+
+public static class ServiceConfiguration
+{
+    public static HostApplicationBuilder ConfigureServices(this HostApplicationBuilder builder, IConfiguration configuration)
+    {
+        // builder.Logging.AddConsole(consoleLogOptions =>
+        // {
+        //     // Configure all logs to go to stderr
+        //     consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+        // });
+
+        // builder.Services
+        //     .AddMcpServer()
+        //     .WithStdioServerTransport()
+        //     .WithTools<FtpTool>();
+
+        return builder;
+    }
+}
